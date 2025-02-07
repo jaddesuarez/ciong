cb0439bbe2c3e404c4369c1605501a2d7b23df48
Thales-eth <dan.console.log@gmail.com>
2025-02-07 18:02:22 +0100

    init astro project

---
+++ src/consts/index.ts
@@ -0,0 +1,22 @@
+export const NAVLINKS = [
+  {
+    href: "/",
+    text: "La esencia",
+  },
+  {
+    href: "/",
+    text: "¿Qué es?",
+  },
+  {
+    href: "/",
+    text: "Tipos",
+  },
+  {
+    href: "/",
+    text: "Requisitos para ser amado",
+  },
+  {
+    href: "/",
+    text: "Recursos",
+  },
+]
